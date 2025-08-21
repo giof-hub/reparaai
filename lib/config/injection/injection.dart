288677ef16abbf0e42f7e44097482aae418e0a84
Giovanni <giovanni.5160@aluno.pr.senac.br>
2025-08-21 16:37:09 -0300

    first commit

---
+++ lib/config/injection/injection.dart
@@ -0,0 +1,27 @@
+import 'package:reparaai/config/injection/base_injection.dart';
+
+class Injection extends BaseInjection{
+  @override
+  void registerControllers() {
+  }
+
+  @override
+  void registerDataSources() {
+  }
+
+  @override
+  void registerExternals() {
+  }
+
+  @override
+  void registerRepositories() {
+  }
+
+  @override
+  void registerSingletons() {
+  }
+
+  @override
+  void registerUseCases() {
+  }
+}
