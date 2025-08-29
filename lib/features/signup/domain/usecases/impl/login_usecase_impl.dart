cae8ab8a33a659bd7af261a9455f387a3e52dc5a
GIOVANNI FELICIO CARVALHO BORGES <giovanni.5160@aluno.pr.senac.br>
2025-08-28 21:51:24 -0300

    Refatoraçõa para módulo interno

---
+++ lib/features/signup/domain/usecases/impl/login_usecase_impl.dart
@@ -0,0 +1,9 @@
+import 'package:reparaai/domain/entities/login_entity.dart';
+import 'package:reparaai/domain/usecases/login_usecase.dart';
+
+class LoginUsecaseImpl implements LoginUsecase {
+  @override
+  void signin(LoginEntity login) {
+    // TODO: implement signin
+  }
+}
---
+++ lib/features/signup/presentation/controllers/login_controller.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+import 'package:reparaai/domain/entities/login_entity.dart';
+import 'package:reparaai/domain/usecases/login_usecase.dart';
+
+part 'login_controller.g.dart';
+
+class LoginController = _LoginController with _$LoginController;
+
+abstract class _LoginController with Store {
+  final LoginUsecase _useCase;
+
+  _LoginController(this._useCase);
+
+  @observable
+  LoginEntity login = LoginEntity.empty();
+
+  void signin() {
+    _useCase.signin(login);
+  }
+}
